bd3ac3aee9ba9a870850a81659bd746e461b6608
Azadpour <eazadpour@usgs.gov>
2022-08-24 16:03:06 -0700

    updated roxygen skeleton to reflect param and export changes, updated function inputs/outputs to reflect object instead of file format in targets.R

--- 1_fetch/src/get_nwis_data.R
+++ 1_fetch/src/get_nwis_data.R
@@ -1,14 +1,14 @@
 #' Getting NWIS Data 
 #'
 #' @param site_num character, a site number for a USGS streamgage
-#' @param file_out csv, csv exported for each designated site_num
+#' @param file_out csv, csv exported with data on all 4 site_num
 #' @param parameterCd, parameter code corresponding to the type of data queried
 #' @param startDate 
 #' @param endDate 
 #'
-#' @export csvs,export 5 csvs: site_info.csv, nwis_01427207_data.csv, nwis_01432160_data.csv, nwis_01436690_data.csv, & nwis_01466500_data.csv. 
+#' @export csvs,export 2 csvs: site_data.csv and site_info.csv
 
-download_nwis_site_data <- function(file_out, site_num, parameterCd, startDate, endDate){
+download_nwis_site_data <- function(site_num, parameterCd, startDate, endDate){
 
   # readNWISdata is from the dataRetrieval package
   data_out <- readNWISdata(sites=site_num, service="iv", 
@@ -21,18 +21,19 @@ download_nwis_site_data <- function(file_out, site_num, parameterCd, startDate, 
   }
   # -- end of do-not-edit block
   
-  write_csv(data_out, file = file_out)
-  return(file_out)
+  return(data_out)
 }
 
-# bind csvs so we can get site_info  
-sites_csvs <- function(file_in){
-  lapply(file_in, read_csv) %>% 
-    bind_rows()
+# bind data so we can get site_info  
+sites <- function(input, file_out){
+    bind_rows(input) %>% 
+    write_csv(file = file_out)
+  return(file_out)
 }
 
 # obtain site info, includes cols such as: station_nm, site_tp, lat_va, long_va, etc.
 nwis_site_info <- function(file_out, site_data){
+  site_data <- read_csv(site_data)
   site_no <- unique(site_data$site_no)
   site_info <- dataRetrieval::readNWISsite(site_no)
   write_csv(site_info, file_out)
